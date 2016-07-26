e562c84e2adca04623d7d2eeba7b11c0dc9dcbf8
Kumar Gaurav <kumar.gaurav@synerzip.com>
2016-07-26 13:08:38 +0530

    Modified styles

--- src/js/app/appConfig.js
+++ src/js/app/appConfig.js
@@ -1,40 +1,43 @@
 define([], function(){
 
-  'use strict';
+    'use strict';
 
-  var appConfig = ['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.rule(function ($injector, $location) {
-      var path = $location.url();
-      // check to see if the path has a trailing slash
-      if ('/' === path[path.length - 1]) {
-        return path.replace(/\/$/, '');
-      }
-      return false;
-    });
-    $urlRouterProvider
-      .when('/','home')
-      .otherwise('home');
-
-    $stateProvider
-        .state('default',{
-          url: '/',
-          views:{
-            '': {
-              templateUrl: 'partials/defaultAppPage.html'
+    var appConfig = ['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.rule(function ($injector, $location) {
+            var path = $location.url();
+            // check to see if the path has a trailing slash
+            if ('/' === path[path.length - 1]) {
+                return path.replace(/\/$/, '');
             }
-          }
-        })
-        .state('default.home',{
-          url: 'home',
-          views:{
-            'moduleContainer@default':{
-              templateUrl: 'partials/home.html',
-              controller: 'homeController'
-            }
-          }
+            return false;
         });
-  }];
+        $urlRouterProvider
+            .when('/','home')
+            .otherwise('home');
 
-  return appConfig;
+        $stateProvider
+            .state('default',{
+                url: '/',
+                views:{
+                    '': {
+                        templateUrl: 'partials/defaultAppPage.html'
+                    }
+                }
+            })
+            .state('default.home',{
+                url: 'home',
+                views:{
+                    'moduleContainer@default':{
+                        templateUrl: 'partials/home.html',
+                        controller: 'homeController'
+                    },
+                    'footer@default':{
+                        templateUrl: 'partials/footer.html'
+                    }
+                }
+            });
+    }];
+
+    return appConfig;
 
 });
